35911f790964160c17f198796f6c81bab3d7a0b4
Erik Telepovský <erik.telepovsky@gmail.com>
2014-07-09 15:27:35 +0200

    v0.6.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-clever-selects',
-    version='0.6.7',
+    version='0.6.8',
     description='Chained select box widget for Django framework using AJAX requests.',
     long_description=open('README.rst').read(),
     author='Pragmatic Mates',
